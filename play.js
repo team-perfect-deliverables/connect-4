6c3ace4666905a39782d066391c9fa4fcbf40446
Jacob Leach <leach.jacob@gmail.com>
2014-10-29 16:31:35 -0400

    Wrote play function

---
+++ play.js
@@ -0,0 +1,14 @@
+
+
+function play(state, column, color)
+{
+  var depth = 0;
+  while(state[depth][column] == null)
+  {
+    depth++; 
+  }
+
+  state[depth - 1][column] = color;
+  
+  return state;
+}
